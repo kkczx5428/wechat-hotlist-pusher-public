e082351be53b9b680b291f522059cfb353647c4f
xaoyaoo <xaoyaoo@qq.com>
2024-04-19 17:23:11 +0800

    UI 偏移地址获取 fix

--- src/views/tools/BiasView.vue
+++ src/views/tools/BiasView.vue
@@ -12,6 +12,11 @@ const result = ref<string>(''); // 结果
 
 const decrypt = async () => {
   try {
+    // key与wxdbPath二选一
+    if (key.value === '' && wxdbPath.value === '') {
+      result.value = 'key与wxdbPath必须填写一个';
+      return;
+    }
     result.value = await http.post('/api/biasaddr', {
       mobile: mobile.value,
       name: name.value,
@@ -19,6 +24,7 @@ const decrypt = async () => {
       key: key.value,
       wxdbPath: wxdbPath.value
     });
+    result.value = "{版本号:昵称,账号,手机号,邮箱,KEY}\n"+result.value;
   } catch (error) {
     console.error('Error fetching data:', error);
     return [];
@@ -41,23 +47,23 @@ const decrypt = async () => {
         <el-input placeholder="请输入手机号" v-model="mobile" style="width: 50%;"></el-input>
         <br>
         <label>昵称: </label>
-        <el-input placeholder="请输入姓名" v-model="name" style="width: 50%;"></el-input>
+        <el-input placeholder="请输入昵称" v-model="name" style="width: 50%;"></el-input>
         <br>
         <label>微信账号: </label>
         <el-input placeholder="请输入微信号" v-model="account" style="width: 50%;"></el-input>
         <br>
         <label>密钥（key）: </label>
-        <el-input placeholder="请输入密钥（key）" v-model="key" style="width: 50%;"></el-input>
+        <el-input placeholder="请输入密钥（key）（可选）" v-model="key" style="width: 50%;"></el-input>
         <br>
         <label>微信数据库路径: </label>
-        <el-input placeholder="请输入微信数据库路径" v-model="wxdbPath" style="width: 50%;"></el-input>
+        <el-input placeholder="请输入微信数据库路径（可选）" v-model="wxdbPath" style="width: 50%;"></el-input>
         <br>
         <el-button style="margin-top: 10px;width: 50%;" type="success" @click="decrypt">偏移</el-button>
         <!--    分割线    -->
         <el-divider></el-divider>
         <!--    分割线    -->
-        <el-input type="textarea" :rows="10" readonly placeholder="解密后数据库路径" v-model="decryptResult"
-                  style="width: 100%;"></el-input>
+        <el-input type="textarea" :rows="10" readonly placeholder="输出结果" v-model="result"
+                  style="width: 100%;color: #00bd7e;"></el-input>
       </div>
     </div>
   </div>
