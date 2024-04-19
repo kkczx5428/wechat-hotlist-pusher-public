8fca451988d61aae6ce4770315588e7943b1e8d9
xaoyaoo <xaoyaoo@qq.com>
2024-04-19 17:44:36 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import {ref} from 'vue'
 
 const isCollapse = ref(true)
 const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+  // console.log(key, keyPath)
 }
 const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+  // console.log(key, keyPath)
 }
 </script>
 
