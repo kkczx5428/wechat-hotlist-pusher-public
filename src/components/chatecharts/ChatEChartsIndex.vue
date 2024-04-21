ff492dbab9d777adfde6acb67aa7975aedda2d77
xaoyaoo <xaoyaoo@qq.com>
2024-04-21 23:21:48 +0800

    test 聊天记录分析

--- src/components/chatecharts/ChatEChartsIndex.vue
+++ src/components/chatecharts/ChatEChartsIndex.vue
@@ -4,71 +4,91 @@ import {onMounted} from "vue";
 
 const dataCount = 5e5;
 
-
-
 //初始化函数
 function init() {
   // 基于准备好的dom，初始化echarts实例
   let Chart = echarts.init(document.getElementById("charts_main"));
+
   // 绘制图表
-  var options = {
-    title: { // 标题
-      text: echarts.format.addCommas(dataCount) + ' Data',
-      left: 10
+  let base = +new Date(1968, 9, 3);
+  let oneDay = 24 * 3600 * 1000;
+  let date = [];
+
+  let data = [Math.random() * 300];
+
+  for (let i = 1; i < 20000; i++) {
+    var now = new Date((base += oneDay));
+    date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+    data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
+  }
+
+  let option = {
+    tooltip: {
+      trigger: 'axis',
+      position: function (pt) {
+        return [pt[0], '10%'];
+      }
     },
-    toolbox: { // 工具栏
+    title: {
+      left: 'center',
+      text: 'TEST'
+    },
+    toolbox: {
       feature: {
         dataZoom: {
-          yAxisIndex: false
+          yAxisIndex: 'none'
         },
-        saveAsImage: {
-          pixelRatio: 2
-        }
+        restore: {},
+        saveAsImage: {}
       }
     },
-    tooltip: { // 提示框
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow'
-      }
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: date
     },
-    grid: { // 网格
-      bottom: 90
+    yAxis: {
+      type: 'value',
+      boundaryGap: [0, '100%']
     },
-    dataZoom: [ // 数据区域缩放
+    dataZoom: [
       {
-        type: 'inside' // 内置
+        type: 'inside',
+        start: 0,
+        end: 10
       },
       {
-        type: 'slider'  // 滑块
+        start: 0,
+        end: 10
       }
     ],
-    xAxis: { // x轴
-      // data: data.categoryData,
-      silent: false,  // 是否响应鼠标事件
-      splitLine: {
-        show: false // 是否显示分隔线
-      },
-      splitArea: {
-        show: false // 是否显示分隔区域
-      }
-    },
-    yAxis: { // y轴
-      splitArea: {
-        show: false // 是否显示分隔区域
-      }
-    },
-    series: [ // 系列
+    series: [
       {
-        type: 'bar',  // 柱状图
-        // data: data.valueData,  // 数据
-        // Set `large` for large data amount
-        large: true // 是否开启大数据量优化
+        name: 'Fake Data',
+        type: 'line',
+        symbol: 'none',
+        sampling: 'lttb',
+        itemStyle: {
+          color: 'rgb(255, 70, 131)'
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgb(255, 158, 68)'
+            },
+            {
+              offset: 1,
+              color: 'rgb(255, 70, 131)'
+            }
+          ])
+        },
+        data: data
       }
     ]
   };
   // 渲染图表
-  // Chart.setOption(options);
+  Chart.setOption(option);
 }
 
 onMounted(() => {
@@ -83,7 +103,7 @@ onMounted(() => {
 
 <style scoped>
 #charts_main {
-  width: 100vw;
-  height: 100vh;
+  width: 90vw;
+  height: 80vh;
 }
 </style>
