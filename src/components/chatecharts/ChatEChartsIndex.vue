14dcb7694fecab4a768d4a868b3f12585a88aaf3
xaoyaoo <xaoyaoo@qq.com>
2024-04-21 16:07:37 +0800

    增加数据展示的包

---
+++ src/components/chatecharts/ChatEChartsIndex.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import * as echarts from "echarts";
+import {onMounted} from "vue";
+
+const dataCount = 5e5;
+
+
+
+//初始化函数
+function init() {
+  // 基于准备好的dom，初始化echarts实例
+  let Chart = echarts.init(document.getElementById("charts_main"));
+  // 绘制图表
+  var options = {
+    title: { // 标题
+      text: echarts.format.addCommas(dataCount) + ' Data',
+      left: 10
+    },
+    toolbox: { // 工具栏
+      feature: {
+        dataZoom: {
+          yAxisIndex: false
+        },
+        saveAsImage: {
+          pixelRatio: 2
+        }
+      }
+    },
+    tooltip: { // 提示框
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: { // 网格
+      bottom: 90
+    },
+    dataZoom: [ // 数据区域缩放
+      {
+        type: 'inside' // 内置
+      },
+      {
+        type: 'slider'  // 滑块
+      }
+    ],
+    xAxis: { // x轴
+      // data: data.categoryData,
+      silent: false,  // 是否响应鼠标事件
+      splitLine: {
+        show: false // 是否显示分隔线
+      },
+      splitArea: {
+        show: false // 是否显示分隔区域
+      }
+    },
+    yAxis: { // y轴
+      splitArea: {
+        show: false // 是否显示分隔区域
+      }
+    },
+    series: [ // 系列
+      {
+        type: 'bar',  // 柱状图
+        // data: data.valueData,  // 数据
+        // Set `large` for large data amount
+        large: true // 是否开启大数据量优化
+      }
+    ]
+  };
+  // 渲染图表
+  // Chart.setOption(options);
+}
+
+onMounted(() => {
+  init();
+});
+
+</script>
+
+<template>
+  <div id="charts_main"></div>
+</template>
+
+<style scoped>
+#charts_main {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 
+import ChatEChartsIndex from "@/components/chatecharts/ChatEChartsIndex.vue";
+
 </script>
 
 <template>
   <div style="background-color: #d2d2fa; height: 100vh; display: grid; place-items: center; ">
-    <h2 style="text-align: center">欢迎使用<a href="https://github.com/xaoyaoo/PyWxDump.git">PyWxDump</a>聊天记录查看工具!
-    </h2>
-    <h3 style="text-align: center">
-      用于统计微信聊天记录，包括聊天记录数量、聊天记录总字数、聊天记录总图片数量<br>聊天记录总视频数量、聊天记录总文件数量、聊天记录总语音数量等等。
-      行成统计报表，方便用户查看自己的聊天记录情况
-    </h3>
-    <p>如需提前体验更多功能，请多多支持，多多鼓励！</p>
+    <!--    <h2 style="text-align: center">欢迎使用<a href="https://github.com/xaoyaoo/PyWxDump.git">PyWxDump</a>聊天记录查看工具!-->
+    <!--    </h2>-->
+    <!--    <h3 style="text-align: center">-->
+    <!--      用于统计微信聊天记录，包括聊天记录数量、聊天记录总字数、聊天记录总图片数量<br>聊天记录总视频数量、聊天记录总文件数量、聊天记录总语音数量等等。-->
+    <!--      行成统计报表，方便用户查看自己的聊天记录情况-->
+    <!--    </h3>-->
+    <ChatEChartsIndex/>
   </div>
 </template>
 
