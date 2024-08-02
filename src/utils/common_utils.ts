726d32e4783d8090e1ff3c2f2ebf5ce94448d74e
xaoyaoo <xaoyaoo@163.com>
2024-08-02 11:01:15 +0800

    初始化数据完成

--- src/utils/common_utils.ts
+++ src/utils/common_utils.ts
@@ -1,7 +1,21 @@
 import {ElMessage} from "element-plus";
 import router from "@/router";
+import http from '@/utils/axios.js';
 
-export const is_db_init = () => {
+export const is_db_init = async () => {
+    try {
+        const res = await http.get('/api/rs/is_init'); // 使用 await 来等待异步结果
+        console.log("is_db_init", res);
+        if (res) {
+            localStorage.setItem('isDbInit', 't');
+        } else {
+            localStorage.setItem('isDbInit', 'f');
+        }
+    } catch (err) {
+        localStorage.setItem('isDbInit', 'f');
+        console.log(err);
+    }
+
     if (localStorage.getItem('isDbInit') === 't') {
         return true;
     } else {
